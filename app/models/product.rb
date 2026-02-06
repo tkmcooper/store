22eef1478bf0645f776ab32f48371e871c6b6fa4
Terry Cooper <coopertkm@gmail.com>
2026-02-06 10:54:05 +0000

    Additional touches

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
-class Product < ApplicationRecord include Notifications
-  has_many :subscribers, dependent: :destroy
+class Product < ApplicationRecord
+  include Product::Notifications
+
   has_one_attached :featured_image
   has_rich_text :description
 
