61c844b20524592228b7d0f9bb004e9f614d59d1
Terry Cooper <coopertkm@gmail.com>
2026-02-06 07:57:15 +0000

    Subscribe & Unsubscribe

---
+++ app/controllers/unsubscribe_controller.rb
@@ -0,0 +1,14 @@
+class UnsubscribeController < ApplicationController
+  allow_unauthenticated_access
+  before_action :set_subscriber
+
+  def show
+    @subscribe&.destroy
+    redirect_to root_path, notice: "Unsubscribed successfully."
+  end
+
+  private
+    def set_subscriber
+      @subscriber = Subscriber.find_by_token_for(:unsubscribe, params[:token])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :subscribers, only: [ :create ]
   end
+  resource :unsubscribe, only: [ :show ]
 
 
   get "up" => "rails/health#show", as: :rails_health_check
